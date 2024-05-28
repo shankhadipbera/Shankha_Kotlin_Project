1723d2cbe9779d6c03bb51936aae20fa2375d31e
Shankhadip Bera <shankha181@gmail.com>
2024-05-28 19:27:18 +0530

    version 3

--- WeatherNow/app/build.gradle.kts
+++ WeatherNow/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.shankha.weathernow"
         minSdk = 28
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
